25604f9671882273eb6ab816fd964544c02021bf
Sosuwake <102490516+Sosuwake@users.noreply.github.com>
2022-05-30 08:13:39 +0100

    Delete app.js

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import {sum} from './gameadd';
-
-export const firstEx = (add) => {
-  const val1 = Math.floor(Math.random() * 9) + 1;
-  const val2 = Math.floor(Math.random() * 9) + 1;
-  Display.create(val1, val2, add);
-  return sum(val1, val2);
-};
-
