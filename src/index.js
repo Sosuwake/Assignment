ddc817adb419ff09a2d8284fc64d9e67c36e914b
Sosuwake <102490516+Sosuwake@users.noreply.github.com>
2022-06-08 14:24:54 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import '../css/index.css';
+import Game from "./game.js"
+import {Display} from "./display.js";
+const game = new Game();
+const display = new Display();
+
+display.render('setup');
+
+document.querySelector('#root').addEventListener('keyup', (e) => {
+  const target = e.target;
+  if (target.matches("#answer-input")) {
+      game.validateResult(document.querySelector('#answer-input').value);
+  }
+});
+
+document.querySelector('#root').addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.matches(".start-button")) {
+      game.start();
+  }
+});
+
