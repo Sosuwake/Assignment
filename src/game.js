e223c27e504aa8fa0ea8c3e3438c36bdec0c9f11
Sosuwake <102490516+Sosuwake@users.noreply.github.com>
2022-06-08 14:22:11 +0100

    Delete src directory

--- src/game.js
+++
@@ -1,9 +0,0 @@
-import {Display} from './display';
-
-export const firstEx = (game) => {
-  const firstNum = Math.floor((Math.random() * 9) + 1);
-  const secondNum = Math.floor((Math.random() * 9) + 1);
-  Display.create(firstNum, secondNum, game);
-  return sum(firstNum, secondNum);
-};
-
